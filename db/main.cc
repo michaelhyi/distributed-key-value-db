62e182183a825da1f602683d067c0429138a74ee
michaelhyi <michaelyi2004@gmail.com>
2025-08-13 16:38:01 -0700

    updated db service with new RunServer impl

--- db/main.cc
+++ db/main.cc
@@ -5,13 +5,15 @@
 #include "db.h"
 #include "db_util.h"
 
+const int GRPC_SERVER_PORT = 50051;
+
 int main(int argc, char **argv) {
     spdlog::cfg::load_env_levels();
 
     DbUtil::register_node();
 
     DbImpl service;
-    RunServer(&service);
+    RunServer(GRPC_SERVER_PORT, &service);
 
     return 0;
 }
